73486428e9ec494dce13750a36dc3bfed8b064a1
Dariusz Talach <dar.talach@gmail.com>
2023-04-11 16:55:39 +0200

    Code rework adding functions + small fixes

--- js/script.js
+++ js/script.js
@@ -1,193 +1,112 @@
-console.log("Hello :D");
+{
+  console.log("Hello :D");
 
-let formElement = document.querySelector(".js-form");
-let fromCurrency = document.querySelector(".js-fromCurrency");
-let toCurrency = document.querySelector(".js-toCurrency");
-let inputAmount = document.querySelector(".js-inputAmount");
-let currencyResult = document.querySelector(".js-result");
+  const calculateResult = (amount, fromCurrencyEL, toCurrencyEL) => {
+    const ratePLN_EUR = 0.21;
+    const ratePLN_USD = 0.23;
+    const ratePLN_GBP = 0.19;
+    const ratePLN_JPY = 30.7;
 
-let ratePLN_EUR = 0.21;
-let ratePLN_USD = 0.23;
-let ratePLN_GBP = 0.19;
-let ratePLN_JPY = 30.7;
+    const rateEUR_PLN = 4.69;
+    const rateEUR_USD = 1.07;
+    const rateEUR_GBP = 0.89;
+    const rateEUR_JPY = 143.85;
 
-let rateEUR_PLN = 4.69;
-let rateEUR_USD = 1.07;
-let rateEUR_GBP = 0.89;
-let rateEUR_JPY = 143.85;
+    const rateUSD_PLN = 4.43;
+    const rateUSD_EUR = 0.95;
+    const rateUSD_GBP = 0.84;
+    const rateUSD_JPY = 136.15;
 
-let rateUSD_PLN = 4.43;
-let rateUSD_EUR = 0.95;
-let rateUSD_GBP = 0.84;
-let rateUSD_JPY = 136.15;
+    const rateGBP_PLN = 5.27;
+    const rateGBP_EUR = 1.12;
+    const rateGBP_USD = 1.18;
+    const rateGBP_JPY = 161.75;
 
-let rateGBP_PLN = 5.27;
-let rateGBP_EUR = 1.12;
-let rateGBP_USD = 1.18;
-let rateGBP_JPY = 161.75;
+    const rateJPY_PLN = 0.033;
+    const rateJPY_EUR = 0.007;
+    const rateJPY_USD = 0.0073;
+    const rateJPY_GBP = 0.0062;
 
-let rateJPY_PLN = 0.033;
-let rateJPY_EUR = 0.007;
-let rateJPY_USD = 0.0073;
-let rateJPY_GBP = 0.0062;
-
-formElement.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  let amount = +inputAmount.value;
-  let result = 0;
-
-  switch (fromCurrency.value) {
-    case "PLN":
-      switch (toCurrency.value) {
-        case "EUR": {
-          result = amount * ratePLN_EUR;
-          break;
+    switch (fromCurrencyEL) {
+      case "PLN":
+        switch (toCurrencyEL) {
+          case "EUR": return amount * ratePLN_EUR;
+          case "USD": return amount * ratePLN_USD;
+          case "GBP": return amount * ratePLN_GBP;
+          case "JPY": return amount * ratePLN_JPY;
+          default: return amount * 1;
         }
 
-        case "USD": {
-          result = amount * ratePLN_USD;
-          break;
+      case "EUR":
+        switch (toCurrencyEL) {
+          case "PLN": return amount * rateEUR_PLN;
+          case "USD": return amount * rateEUR_USD;
+          case "GBP": return amount * rateEUR_GBP;
+          case "JPY": return amount * rateEUR_JPY;
+          default: return amount * 1;
         }
 
-        case "GBP": {
-          result = amount * ratePLN_GBP;
-          break;
+      case "USD":
+        switch (toCurrencyEL) {
+          case "PLN": return amount * rateUSD_PLN;
+          case "EUR": return amount * rateUSD_EUR;
+          case "GBP": return amount * rateUSD_GBP;
+          case "JPY": return amount * rateUSD_JPY;
+          default: return amount * 1;
         }
 
-        case "JPY": {
-          result = amount * ratePLN_JPY;
-          break;
+      case "GBP":
+        switch (toCurrencyEL) {
+          case "PLN": return amount * rateGBP_PLN;
+          case "EUR": return amount * rateGBP_EUR;
+          case "USD": return amount * rateGBP_USD;
+          case "JPY": return amount * rateGBP_JPY;
+          default: return amount * 1;
         }
 
-        default: {
-          result = amount * 1;
-          break;
+      case "JPY":
+        switch (toCurrencyEL) {
+          case "PLN": return amount * rateJPY_PLN;
+          case "EUR": return amount * rateJPY_EUR;
+          case "USD": return amount * rateJPY_USD;
+          case "GBP": return amount * rateJPY_GBP;
+          default: return amount * 1;
         }
-      }
-      break;
+    }
+  };
 
-    case "EUR":
-      switch (toCurrency.value) {
-        case "PLN": {
-          result = amount * rateEUR_PLN;
-          break;
-        }
-
-        case "USD": {
-          result = amount * rateEUR_USD;
-          break;
-        }
-
-        case "GBP": {
-          result = amount * rateEUR_GBP;
-          break;
-        }
-
-        case "JPY": {
-          result = amount * rateEUR_JPY;
-          break;
-        }
-
-        default: {
-          result = amount * 1;
-          break;
-        }
-      }
-      break;
-
-    case "USD":
-      switch (toCurrency.value) {
-        case "PLN": {
-          result = amount * rateUSD_PLN;
-          break;
-        }
-
-        case "EUR": {
-          result = amount * rateUSD_EUR;
-          break;
-        }
-
-        case "GBP": {
-          result = amount * rateUSD_GBP;
-          break;
-        }
-
-        case "JPY": {
-          result = amount * rateUSD_JPY;
-          break;
-        }
-
-        default: {
-          result = amount * 1;
-          break;
-        }
-      }
-      break;
-
-    case "GBP":
-      switch (toCurrency.value) {
-        case "PLN": {
-          result = amount * rateGBP_PLN;
-          break;
-        }
-
-        case "EUR": {
-          result = amount * rateGBP_EUR;
-          break;
-        }
-
-        case "USD": {
-          result = amount * rateGBP_USD;
-          break;
-        }
-
-        case "JPY": {
-          result = amount * rateGBP_JPY;
-          break;
-        }
-
-        default: {
-          result = amount * 1;
-          break;
-        }
-      }
-      break;
-
-    case "JPY":
-      switch (toCurrency.value) {
-        case "PLN": {
-          result = amount * rateJPY_PLN;
-          break;
-        }
-
-        case "EUR": {
-          result = amount * rateJPY_EUR;
-          break;
-        }
-
-        case "USD": {
-          result = amount * rateJPY_USD;
-          break;
-        }
-
-        case "GBP": {
-          result = amount * rateJPY_GBP;
-          break;
-        }
-
-        default: {
-          result = amount * 1;
-          break;
-        }
-      }
-      break;
+  const updateResultText = (amount, result, fromCurrencyEL, toCurrencyEL) => {
+    const currencyResult = document.querySelector(".js-result");
+    currencyResult.innerText = `${amount.toFixed(2)} ${fromCurrencyEL} = ${result.toFixed(2)} ${toCurrencyEL}`;
   }
 
-  currencyResult.innerText = `${amount} ${fromCurrency.value} = 
-  ${result.toFixed(2)} ${toCurrency.value}`;
-});
+  const onFormSubmit = (event) => {
+      event.preventDefault();
+    
+      const fromCurrency = document.querySelector(".js-fromCurrency");
+      const toCurrency = document.querySelector(".js-toCurrency");
+      const inputAmount = document.querySelector(".js-inputAmount");
+  
+      const amount = +inputAmount.value;
+      const fromCurrencyEL = fromCurrency.value;
+      const toCurrencyEL = toCurrency.value;
+  
+      let result = calculateResult(amount, fromCurrencyEL, toCurrencyEL);
+  
+      updateResultText(amount, result, fromCurrencyEL, toCurrencyEL);
+  }
 
-formElement.addEventListener("reset", () => {
-  currencyResult.innerText = "0.00";
-});
+  const init = () => {
+    const formElement = document.querySelector(".js-form");
+
+    formElement.addEventListener("submit", onFormSubmit);
+
+    formElement.addEventListener("reset", () => {
+      currencyResult.innerText = "0.00";
+      console.clear();
+    }
+  )};
+
+
+  init();
+}
